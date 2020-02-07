84383f4ae017fc4a1efadcf0a6575942dd75aecb
AeDani <31882578+AeDani@users.noreply.github.com>
2020-02-07 21:52:45 +0100

    move express.json on above router

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const taskRouter = require('./router/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-app.use(express.json());
 
 app.listen(port, () => {
 	console.log(`Express server started on port ${port}`);
