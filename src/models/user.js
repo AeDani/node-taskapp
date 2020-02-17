227eb5fd3959448dd9329e3d1db237e8c0abcdef
AeDani <31882578+AeDani@users.noreply.github.com>
2020-02-17 21:38:07 +0100

    tasks route with sorting, pagination and filtering

--- src/models/user.js
+++ src/models/user.js
@@ -4,53 +4,58 @@ const bcrypt = require('bcryptjs');
 const Task = require('./task');
 const jwt = require('jsonwebtoken');
 
-const userSchema = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is not valid');
+const userSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+			lowercase: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error('Email is not valid');
+				}
 			}
-		}
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 7,
-		validate(value) {
-			if (value.toLowerCase().includes('password')) {
-				throw new Error('Password can not contain "password"');
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 7,
+			validate(value) {
+				if (value.toLowerCase().includes('password')) {
+					throw new Error('Password can not contain "password"');
+				}
 			}
-		}
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must be positive.');
+		},
+		age: {
+			type: Number,
+			default: 0,
+			validate(value) {
+				if (value < 0) {
+					throw new Error('Age must be positive.');
+				}
 			}
-		}
-	},
-	tokens: [
-		{
-			token: {
-				type: String,
-				required: true
+		},
+		tokens: [
+			{
+				token: {
+					type: String,
+					required: true
+				}
 			}
-		}
-	]
-});
+		]
+	},
+	{
+		timestamps: true
+	}
+);
 
 // Generate jwt
 userSchema.methods.generateAuthToken = async function() {
